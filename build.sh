d6e50698bc0eede89c7678b91bcacbb66e813b32
mpolatcan <mutlupolatcan@gmail.com>
2019-10-07 22:47:13 +0300

    hadoop versions updated

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 HADOOP_VERSIONS=(
-    "3.2.0"
+    "3.2.1"
     "3.1.2"
-    "3.0.3"
-    "2.9.1"
-    "2.8.4"
-    "2.7.6"
-    "2.6.5"
+    "2.9.2"
+    "2.8.5"
+    "2.7.7"
 )
 
 DISTS=(
@@ -19,8 +17,8 @@ DISTS=(
 # $2: HADOOP_VERSION
 function build_image() {
     sudo docker build -q -t mpolatcan/hadoop:$1-$2 --build-arg HADOOP_VERSION=$2 ./$1/
-	sudo docker push mpolatcan/hadoop:$1-$2
-	sudo docker rmi mpolatcan/hadoop:$1-$2
+	  sudo docker push mpolatcan/hadoop:$1-$2
+	  sudo docker rmi mpolatcan/hadoop:$1-$2
 }
 
 for HADOOP_VERSION in ${HADOOP_VERSIONS[@]}; do
