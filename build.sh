0f849422a15d60e1c0d701421f962695a313202d
mpolatcan <mutlupolatcan@gmail.com>
2019-10-08 10:03:41 +0300

    java versions added

--- build.sh
+++ build.sh
@@ -8,20 +8,24 @@ HADOOP_VERSIONS=(
     "2.7.7"
 )
 
-DISTS=(
-    "debian"
+JAVA_VERSIONS=(
+    "8"
+    "9"
+    "10"
+    "11"
+    "12"
 )
 
-# $1: DIST
-# $2: HADOOP_VERSION
+# $1: HADOOP_VERSION
+# $2: JAVA_VERSION
 function build_image() {
-    sudo docker build -q -t mpolatcan/hadoop:$1-$2 --build-arg HADOOP_VERSION=$2 ./$1/
-	  sudo docker push mpolatcan/hadoop:$1-$2
-	  sudo docker rmi mpolatcan/hadoop:$1-$2
+    sudo docker build -q -t mpolatcan/hadoop:$1-java$2 --build-arg HADOOP_VERSION=$1 --build-arg JAVA_VERSION=$2 ./src
+	  sudo docker push mpolatcan/hadoop:$1-java$2
+	  sudo docker rmi mpolatcan/hadoop:$1-java$2
 }
 
 for HADOOP_VERSION in ${HADOOP_VERSIONS[@]}; do
-    for DIST in ${DISTS[@]}; do
-        build_image $DIST $HADOOP_VERSION
-    done
+  for JAVA_VERSION in ${JAVA_VERSIONS[@]}; do
+        build_image $HADOOP_VERSION $JAVA_VERSION
+  done
 done
