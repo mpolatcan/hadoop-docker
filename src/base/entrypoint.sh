1bcd53a0808cd978c658585f562d4e03d968f7e6
mpolatcan <mutlupolatcan@gmail.com>
2019-11-12 14:20:26 +0300

    other hadoop daemons integrated to entrypoint.sh and can be configure from HADOOP_ADDITIONAL_DAEMONS environment variable

--- src/base/entrypoint.sh
+++ src/base/entrypoint.sh
@@ -1,33 +1,76 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+declare -A daemons
+
+# ------- YARN Daemons ---------
+daemons[nodemanager]=yarn
+daemons[proxyserver]=yarn
+daemons[registrydns]=yarn
+daemons[resourcemanager]=yarn
+daemons[router]=yarn
+daemons[sharedcachemanager]=yarn
+# ------------------------------
+
+# -------- HDFS Daemons --------
+daemons[balancer]=hdfs
+daemons[datanode]=hdfs
+daemons[dfsrouter]=hdfs
+daemons[diskbalancer]=hdfs
+daemons[journalnode]=hdfs
+daemons[mover]=hdfs
+daemons[namenode]=hdfs
+daemons[nfs3]=hdfs
+daemons[portmap]=hdfs
+daemons[secondarynamenode]=hdfs
+daemons[sps]=hdfs
+daemons[zkfc]=hdfs
+# ------------------------------
+
+# ------ MapReduce Daemons ------
+daemons[historyserver]=mapred
+# -------------------------------
+
+function start_daemon() {
+    ${daemons[$1]} --daemon start $1
+}
+
 function start_namenode() {
     # Formatting HDFS
     [[ ! -d "${HADOOP_TMP_DIR}/dfs" ]] && hdfs namenode -format
 
     # Start Hadoop Namenode
-    hdfs --daemon start namenode
+    start_daemon namenode
 
     # Start Hadoop Secondary namenode
-    hdfs --daemon start secondarynamenode
+    start_daemon secondarynamenode
 
     # Start Hadoop Datanode
-    hdfs --daemon start datanode
+    start_daemon datanode
 
     # Start YARN Resourcemanager
-    yarn --daemon start resourcemanager
+    start_daemon resourcemanager
 
     # Start YARN Nodemanager
-    yarn --daemon start nodemanager
+    start_daemon nodemanager
 
     # Start JobHistory server
-    mapred --daemon start historyserver
+    start_daemon historyserver
 }
 
 function start_datanode() {
     # Start Datanode
-    hdfs --daemon start datanode
+    start_daemon datanode
 
     # Start YARN Nodemanager
-    yarn --daemon start nodemanager
+    start_daemon nodemanager
+}
+
+function run_additional_daemons() {
+  HADOOP_ADDITIONAL_DAEMONS=(${HADOOP_ADDITIONAL_DAEMONS//,/ })
+
+  for daemon in ${HADOOP_ADDITIONAL_DAEMONS[@]}; do
+    start_daemon $daemon
+  done
 }
 
 # Load Hadoop configs
@@ -36,6 +79,8 @@ function start_datanode() {
 [[ "${HADOOP_NODE_TYPE}" == "namenode" ]] && start_namenode
 
 [[ "${HADOOP_NODE_TYPE}" == "datanode" ]] && start_datanode
+
+[[ "${HADOOP_ADDITIONAL_DAEMONS}" != "NULL" ]] && run_additional_daemons
 
 if [[ "$1" == "hadoop" ]]; then
     tail -f /dev/null
