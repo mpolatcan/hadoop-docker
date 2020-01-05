44e3886fe3eb52b470238b43d7553228e6f79fb4
mpolatcan <mutlupolatcan@gmail.com>
2020-01-05 23:51:53 +0300

    hadoop daemon healthchecks added

--- src/base/entrypoint.sh
+++ src/base/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# TODO All healthchecks will be added
+HADOOP_DAEMON_NODEMANAGER="nodemanager"
+HADOOP_DAEMON_DATANODE="datanode"
 
 declare -A daemons
+declare -A daemon_hosts
+declare -A daemon_ports
+declare -A healthchecks
 
+# ------------------------- DAEMONS -----------------------
 # ------- YARN Daemons ---------
 daemons[nodemanager]=yarn
 daemons[proxyserver]=yarn
@@ -28,18 +35,66 @@ daemons[zkfc]=hdfs
 
 # ------ MapReduce Daemons ------
 daemons[historyserver]=mapred
-# -------------------------------
+# -------------------------------------------------------------
+
+# ------------------------ HEALTHCHECKS -----------------------
+healthchecks[nodemanager]=resourcemanager
+healthchecks[datanode]=namenode
+# -------------------------------------------------------------
+
+# ------------------------- DAEMON HOSTS ----------------------
+daemon_hosts[resourcemanager]=$YARN_RESOURCEMANAGER_HOSTNAME
+daemon_hosts[namenode]=$DFS_NAMENODE_HOSTNAME
+# -------------------------------------------------------------
+
+# ------------------------ DAEMON PORTS -----------------------
+daemon_ports[resourcemanager]=$YARN_RESOURCEMANAGER_WEBAPP_PORT
+daemon_ports[namenode]=$DFS_NAMENODE_HTTP_PORT
+# -------------------------------------------------------------
+
+function health_checker() {
+  host=${daemon_hosts[$2]}
+
+  if [[ "${daemon_hosts[$2]}" != "NULL" ]]; then
+    host=$HOSTNAME
+  fi
+
+  port=${daemon_ports[$2]}
+
+  echo "Hadoop $2 healthcheck started (host: \"$host\", port: \"$port\")"
+
+  nc $host $port
+  result=$?
+
+  until [[ $result -eq 0 ]]; do
+    echo "Waiting Hadoop $2 is ready (host: \"$host\", port: \"$port\")..."
+    sleep 2
+
+    nc $host $port
+    result=$?
+  done
+
+  echo "Hadoop $2 is ready (host: \"$host\", port: \"$port\") ✔"
+}
 
 function start_daemons() {
   for daemon in ${HADOOP_DAEMONS[@]}; do
       if [[ "$daemon" == "namenode" ]]; then
           # Formatting HDFS
           if [[ ! -d "${HADOOP_TMP_DIR}/dfs" ]]; then
+              echo "Formatting HDFS on namenode..."
               hdfs namenode -format
+          else
+              echo "HDFS already formatted!"
           fi
       fi
 
+      if [[ "${healthchecks[$daemon]}" != "" ]]; then
+        health_checker $daemon ${healthchecks[$daemon]}
+      fi
+
       # Start current daemon
+      echo "Starting Hadoop  $daemon..."
       ${daemons[$daemon]} --daemon start $daemon
   done
 }
